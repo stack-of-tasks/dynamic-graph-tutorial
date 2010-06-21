28d46edb8bfc6db52dac34d69c67fd9da63949a0
florent <florent@laas.fr>
2010-06-21 11:23:46 +0200

    Bind some more methods of InvertedPendulum
    	* src/inverted-pendulum-py.cc,
    	* src/sot_tutorial.py: setCartMass, getPendulumMass, setPendulumMass.

--- src/inverted-pendulum-py.cc
+++ src/inverted-pendulum-py.cc
@@ -11,6 +11,9 @@ using sot::tutorial::InvertedPendulum;
 
 namespace invertedPendulum {
 
+  /**
+     \brief Create an instance of InvertedPendulum
+  */
   static PyObject*
   create(PyObject* self, PyObject* args)
   {
@@ -25,6 +28,9 @@ namespace invertedPendulum {
     return Py_BuildValue("i", (unsigned int)obj);
   }
 
+  /**
+     \brief Deallocate an instance of InvertedPendulum
+  */
   static PyObject*
   destroy (PyObject* self, PyObject* args)
   {
@@ -40,6 +46,9 @@ namespace invertedPendulum {
     return Py_BuildValue("");
   }
 
+  /**
+     \brief Binding for InvertedPendulum::getCartMass
+  */
   static PyObject*
   getCartMass (PyObject* self, PyObject* args)
   {
@@ -54,8 +63,67 @@ namespace invertedPendulum {
 
     return Py_BuildValue("f", result);
   }
+
+  /**
+     \brief Binding for InvertedPendulum::setCartMass
+  */
+  static PyObject*
+  setCartMass (PyObject* self, PyObject* args)
+  {
+    InvertedPendulum* obj = NULL;
+    unsigned int pointer;
+    double value;
+
+    if (!PyArg_ParseTuple(args, "Id", &pointer, &value))
+      return NULL;
+
+    obj = (InvertedPendulum*)pointer;
+    obj->setCartMass(value);
+
+    return Py_BuildValue("");
+  }
+
+  /**
+     \brief Binding for InvertedPendulum::getPendulumMass
+  */
+  static PyObject*
+  getPendulumMass (PyObject* self, PyObject* args)
+  {
+    InvertedPendulum* obj = NULL;
+    unsigned int pointer;
+
+    if (!PyArg_ParseTuple(args, "I", &pointer))
+      return NULL;
+
+    obj = (InvertedPendulum*)pointer;
+    double result = obj->getPendulumMass();
+
+    return Py_BuildValue("f", result);
+  }
+
+  /**
+     \brief Binding for InvertedPendulum::setPendulumMass
+  */
+  static PyObject*
+  setPendulumMass (PyObject* self, PyObject* args)
+  {
+    InvertedPendulum* obj = NULL;
+    unsigned int pointer;
+    double value;
+
+    if (!PyArg_ParseTuple(args, "Id", &pointer, &value))
+      return NULL;
+
+    obj = (InvertedPendulum*)pointer;
+    obj->setPendulumMass(value);
+
+    return Py_BuildValue("");
+  }
 }
 
+/**
+   \brief List of python functions
+*/
 static PyMethodDef sotTutorialMethods[] = {
   {"createInvertedPendulum",  invertedPendulum::create, METH_VARARGS,
      "Create an instance of InvertedPendulum."},
@@ -63,6 +131,12 @@ static PyMethodDef sotTutorialMethods[] = {
      "Destroy an instance of InvertedPendulum."},
   {"invertedPendulumGetCartMass", invertedPendulum::getCartMass, METH_VARARGS,
    "Get mass of the cart of inverted pendulum."},
+  {"invertedPendulumSetCartMass", invertedPendulum::setCartMass, METH_VARARGS,
+   "Set mass of the cart of inverted pendulum."},
+  {"invertedPendulumGetPendulumMass", invertedPendulum::getPendulumMass,
+   METH_VARARGS, "Get mass of the pendulum."},
+  {"invertedPendulumSetPendulumMass", invertedPendulum::setPendulumMass,
+   METH_VARARGS, "Set mass of the pendulum."},
   {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
--- src/sot_tutorial.py
+++ src/sot_tutorial.py
@@ -7,18 +7,40 @@ import sot_tutorial_wrap as stw
 
 class InvertedPendulum:
     """
-    Constructor: create and store a pointer to an C++ object InvertedPendulum
+    This class binds sot::tutorial::InvertedPendulum C++ class
     """
     def __init__(self, name):
+        """
+        Constructor: create and store a pointer to a C++ object InvertedPendulum
+        """
         self.object = stw.createInvertedPendulum(name)
 
-    """
-    Destructor: delete the C++ object allocated at creation
-    """
     def __del__(self):
+        """
+        Destructor: delete the C++ object allocated at creation
+        """
         stw.deleteInvertedPendulum(self.object)
-
+    
     def getCartMass(self):
+        """
+        Get mass of the cart
+        """
         return stw.invertedPendulumGetCartMass(self.object)
 
+    def setCartMass(self, mass):
+        """
+        Set mass of the cart
+        """
+        return stw.invertedPendulumSetCartMass(self.object, mass)
 
+    def getPendulumMass(self):
+        """
+        Get mass of the pendulum
+        """
+        return stw.invertedPendulumGetPendulumMass(self.object)
+
+    def setPendulumMass(self, mass):
+        """
+        Set mass of the pendulum
+        """
+        return stw.invertedPendulumSetPendulumMass(self.object, mass)
