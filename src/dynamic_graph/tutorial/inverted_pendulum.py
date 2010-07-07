d0654d91ba4e5917c50f2513fc041eed1a707140
florent <florent@laas.fr>
2010-07-07 09:31:04 +0200

    Cosmetic change
    	 * src/dynamic_graph/tutorial/inverted_pendulum.py: do not used alias
    	   for module wrap

--- src/dynamic_graph/tutorial/inverted_pendulum.py
+++ src/dynamic_graph/tutorial/inverted_pendulum.py
@@ -3,7 +3,7 @@
 
     Author: Florent Lamiraux
 """
-import wrap as stw
+import wrap
 import dynamic_graph.entity as dge
 
 class InvertedPendulum (dge.Entity):
@@ -16,7 +16,7 @@ class InvertedPendulum (dge.Entity):
         to a C++ InvertedPendulum object.
         """
         if not self.object :
-            self.object = stw.createInvertedPendulum(name)
+            self.object = wrap.createInvertedPendulum(name)
         # Call parent constructor
         dge.Entity.__init__(self, name)
 
@@ -25,48 +25,48 @@ class InvertedPendulum (dge.Entity):
         """
         Get mass of the cart
         """
-        return stw.invertedPendulumGetCartMass(self.object)
+        return wrap.invertedPendulumGetCartMass(self.object)
 
     @cart_mass.setter
     def cart_mass(self, mass):
         """
         Set mass of the cart
         """
-        return stw.invertedPendulumSetCartMass(self.object, mass)
+        return wrap.invertedPendulumSetCartMass(self.object, mass)
 
     @property
     def pendulum_mass(self):
         """
         Get mass of the pendulum
         """
-        return stw.invertedPendulumGetPendulumMass(self.object)
+        return wrap.invertedPendulumGetPendulumMass(self.object)
 
     @pendulum_mass.setter
     def pendulum_mass(self, mass):
         """
         Set mass of the pendulum
         """
-        return stw.invertedPendulumSetPendulumMass(self.object, mass)
+        return wrap.invertedPendulumSetPendulumMass(self.object, mass)
 
     @property
     def pendulum_length(self):
         """
         Get length of the pendulum
         """
-        return stw.invertedPendulumGetPendulumLength(self.object)
+        return wrap.invertedPendulumGetPendulumLength(self.object)
 
     @pendulum_length.setter
     def pendulum_length(self, length):
         """
         Set length of the pendulum
         """
-        return stw.invertedPendulumSetPendulumLength(self.object, length)
+        return wrap.invertedPendulumSetPendulumLength(self.object, length)
 
     def incr(self, timeStep):
         """
         Increment time by one time step and re-compute state accordingly
         """
-        return stw.invertedPendulumIncr(self.object, timeStep)
+        return wrap.invertedPendulumIncr(self.object, timeStep)
 
     def print_state(self) :
-        return stw.invertedPendulumPrintState(self.object)
+        return wrap.invertedPendulumPrintState(self.object)
