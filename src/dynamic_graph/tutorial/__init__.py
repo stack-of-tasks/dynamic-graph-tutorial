5b376d7e34fea6fed8374df56be01ec8b4ee0351
florent <florent@laas.fr>
2010-10-24 21:19:28 +0200

    Move code of python entity class construction to dg-python.
         * src/dynamic_graph/tutorial/__init__.py.

--- src/dynamic_graph/tutorial/__init__.py
+++ src/dynamic_graph/tutorial/__init__.py
@@ -6,24 +6,4 @@
 import dynamic_graph as dg
 import wrap
 
-def initEntity(self, name):
-    """
-    Common constructor of Entity classes
-    """
-    dg.entity.Entity.__init__(self, self.class_name, name)
-
-entityTypeList = dg.factory_get_entity_class_list()
-for e in entityTypeList:
-    class metacls(type):
-        def __new__(mcs, name, bases, dict):
-            return type.__new__(mcs, name, bases, dict)
-
-    # Create new class
-    a = metacls(e, (dg.entity.Entity,), {})
-    # Store class name in class member
-    a.class_name = e
-    print ("new class %s"%e)
-    # set class constructor
-    setattr(a, '__init__', initEntity)
-    # Store new class in dictionary with class name
-    dg.__dict__[e] = a
+dg.entity.updateEntityClasses()
