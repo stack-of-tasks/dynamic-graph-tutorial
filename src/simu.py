3f9b9bc6b47eea9adb02f7afc4df15fe91d4c0fa
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-10-29 15:26:28 +0100

    [Python] Format

--- src/simu.py
+++ src/simu.py
@@ -1,8 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as pl
 import dynamic_graph as dg
 import dynamic_graph.tutorial as dgt
-import dynamic_graph.signal_base as dgsb
+import matplotlib.pyplot as pl
+import numpy as np
 
 # define inverted pendulum
 a = dgt.InvertedPendulum("IP")
@@ -14,8 +13,8 @@ b = dgt.FeedbackController("K")
 
 # plug signals
 stateOut = a.signal('state')
-forceIn =  a.signal('force')
-stateIn =  b.signal('state')
+forceIn = a.signal('force')
+stateIn = b.signal('state')
 forceOut = b.signal('force')
 
 dg.plug(stateOut, stateIn)
@@ -27,20 +26,25 @@ timeStep = 0.001
 s = stateOut
 f = forceIn
 
-s.value = (0.0,0.1,0.0,0.0)
+s.value = (0.0, 0.1, 0.0, 0.0)
+
+gain = ((
+    0.0,
+    27.0,
+    0.001,
+    0.001,
+), )
+b.setGain(gain, )
 
 
-gain = ((0.0,27.0,0.001,0.001,),)
-b.setGain(gain,)
-
-def play (nbSteps):
+def play(nbSteps):
     timeSteps = []
     values = []
     forces = []
 
     # Loop over time and compute discretized state values
-    for x in xrange(nbSteps) :
-        t = x*timeStep
+    for x in range(nbSteps):
+        t = x * timeStep
         timeSteps.append(t)
         values.append(s.value)
         forces.append(f.value)
@@ -50,24 +54,24 @@ def play (nbSteps):
     x = np.array(timeSteps)
     y = np.array(values).transpose()
 
-    fig  = pl.figure()
+    fig = pl.figure()
     ax1 = fig.add_subplot(121)
     ax2 = fig.add_subplot(122)
 
     # plot configuration variables
-    ax1.plot(x,y[0])
-    ax1.plot(x,y[1])
+    ax1.plot(x, y[0])
+    ax1.plot(x, y[1])
 
     # plot velocity variables
-    ax2.plot(x,y[2])
-    ax2.plot(x,y[3])
-    ax2.plot(x,forces)
+    ax2.plot(x, y[2])
+    ax2.plot(x, y[3])
+    ax2.plot(x, forces)
 
-    leg = ax1.legend(("x", "theta"))
-    leg = ax2.legend(("dx", "dtheta", "force"))
+    ax1.legend(("x", "theta"))
+    ax2.legend(("dx", "dtheta", "force"))
 
     pl.show()
 
-if __name__ == '__main__' :
-    play(100)
 
+if __name__ == '__main__':
+    play(100)
